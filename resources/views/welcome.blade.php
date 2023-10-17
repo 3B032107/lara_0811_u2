f179a4c4df3c463177671f8d3fc9b45aa361759a
廖家駒 <3b032107@gm.student.ncut.edu.tw>
2023-10-18 07:12:07 +0800

    為Route命名

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,7 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <a href="{{ route('hello.index') }}">Hello</a>
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
@@ -26,14 +27,11 @@
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                            <a href="{{ route('hello.index') }}">Hello</a>
-                            <a href="{{route('hello.index',['name' => 'tom'])}}">Hello2</a>
-                            <a href="/hello/to m">Hello3</a>
+
                         @endif
                     @endauth
                 </div>
             @endif
-
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
